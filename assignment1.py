83db8c3b4ee562a495be14a3f4bec9dcdb9334cd
jackielongofvietnam <jackielongofvietnam@gmail.com>
2025-06-06 14:27:34 +0700

    aaa

--- assignment1.py
+++ assignment1.py
@@ -39,6 +39,7 @@ fireLabel.pack()
 fanStatus = False  # Track fan state
 
 def toggle_fan():
+    global fanStatus
     if fanStatus:
         command = "FAN_OFF\n"
         arduino.write(command.encode())  # Send command
@@ -49,27 +50,30 @@ def toggle_fan():
         fanButton.config(text="Turn Fan OFF")
 
     fanStatus = not fanStatus # Change fan state every time button is pressed
-    print(fanStatus)
 
 fanButton = Button(root, text="Turn Fan ON", command=toggle_fan, font=("Arial", 12))
 fanButton.pack()
+
+fireStatus = "1"  # Track fire state
 
 # Read Arduino Data
 def update_data():
     arduino.write(b'')
     data = arduino.readline().decode().strip()
     print("Received data:", data)
+    global fireStatus
     if data:
         try:
             temp, fire = data.split(",")
             tempLabel.config(text="Temperature: {} C".format(temp))
-            
-            if fire == "0":
-                fireLabel.config(text="Fire Status: FIRE DETECTED!", fg="red")
-                arduino.write(b'BUZZER_ON\n')  # Buzzer ON
-            else:
-                fireLabel.config(text="Fire Status: No Fire", fg="green")
-                arduino.write(b'BUZZER_OFF\n')  # Buzzer OFF
+            if fire != fireStatus:
+                fireStatus = fire
+                if fire == "0":
+                    fireLabel.config(text="Fire Status: FIRE DETECTED!", fg="red")
+                    arduino.write(b'BUZZER_ON\n')  # Buzzer ON
+                else:
+                    fireLabel.config(text="Fire Status: No Fire", fg="green")
+                    arduino.write(b'BUZZER_OFF\n')  # Buzzer OFF
 
             cursor.execute("INSERT INTO tempLog (temperature, time) VALUES (%s, NOW())", (temp,))
             database.commit()
